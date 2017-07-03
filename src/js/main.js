7df34a8c4c413862e8352fbf7ed76bffa02cdb34
Pahnki Kim <pahnki.kim@pkworks.io>
2017-07-03 10:53:48 +0900

    add webpack.utils.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,4 @@
 require('bootstrap');
+var hello = require('./sample/hello');
+
+$('body').append('<br>' + hello.hello('Webpack'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,32 @@
-const webpack = require('webpack');
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const utils = require('./webpack.utils');
+
+const port = {
+    web: 88,
+    was: 8080
+};
+
+const pages = [{
+    html: 'index',
+    script: 'main',
+    css: 'main'
+}, {
+    html: 'sub',
+    script: 'sub'
+}];
 
 module.exports = {
-    entry: './src/js/main',
+    entry: utils.getEntry(pages),
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: './js/bundle.js'
+        filename: './js/[name].bundle.js'
     },
-    watch: true,
     devServer: {
         contentBase: './dist',
-        port: 88,
+        port: port.web,
         proxy: {
-            '/api': 'http://localhost:8080'
+            '/api': 'http://localhost:' + port.was
         }
     },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new CopyWebpackPlugin([{
-            from: 'src/css',
-            to: 'css/'
-        }, {
-            from: 'node_modules/bootstrap/dist/css',
-            to: 'vendor/bootstrap/css/'
-        }, {
-            from: 'node_modules/bootstrap/dist/fonts',
-            to: 'vendor/bootstrap/fonts/'
-        }]),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery"
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'src/index.html'
-        }),
-        new HtmlWebpackIncludeAssetsPlugin({
-            assets: ['vendor/bootstrap/css/bootstrap.min.css'],
-            append: false
-        }),
-        new HtmlWebpackIncludeAssetsPlugin({
-            assets: ['css/common.css'],
-            append: true
-        }),
-        new HtmlWebpackIncludeAssetsPlugin({
-            files: ['index.html'],
-            assets: ['css/main.css'],
-            append: true
-        })
-    ]
+    plugins: utils.getPlugins(pages)
 };
